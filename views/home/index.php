ecf6a245209f7f3f8e684627175d83491f52490f
Eduardo <dam102.2022@gmail.com>
2025-04-07 10:26:29 +0200

    transParents

--- views/home/index.php
+++ views/home/index.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-	<h1>Hotel Villa de Abalos</h1>
+	<h1 style="color: white;">Hotel Villa de Abalos</h1>
 	<p class="lead">Reserva Ya Aqui</p>
 	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>reservas">Reserva Ya!</a>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/5.11.3/main.min.css">
